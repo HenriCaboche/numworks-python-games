08894f65dc93a039986076dce5992342d137a017
HenriCaboche <henri.caboche.develop@gmail.com>
2025-05-21 13:47:56 +0200

    Added comments for readability

--- DodgeDot.py
+++ DodgeDot.py
@@ -17,27 +17,42 @@ score = ""
 playerSpeed = 5
 
 while running :
+
+    # Update player position
     crateY += crateVelocity
     playerX += playerVelocity
+    
+    # Manages player input
     if ion.keydown(ion.KEY_LEFT):
         playerVelocity = - playerSpeed
     if ion.keydown(ion.KEY_RIGHT):
         playerVelocity = playerSpeed
+
+    # Calculates the distance of the player from the crate
     crateDistance = math.fabs(playerX - crateX)
+
+    # Caps the speed of the crate
     if crateVelocity < 5 :
         crateVelocity += 1
+    
+    # Resets crate position when it touches the ground and increments the score
     if crateY >= 222 - crateSize:
         crateY = 0
         crateX = random.randint(0,320 - playerSize)
         score += "*"
+    
+    # Game Over system
     if crateY >= 222 - crateSize - playerSize and crateDistance <= playerSize:
         running = False
+    # Renders the objects
     kandinsky.fill_rect(playerX,222 - playerSize, playerSize, playerSize,kandinsky.color(0,0,0))
     kandinsky.fill_rect(crateX,crateY, crateSize, crateSize,kandinsky.color(255,0,0))
     kandinsky.draw_string(score,0,0)
     time.sleep(1/60)
     kandinsky.fill_rect(playerX,222 - playerSize, playerSize, playerSize,kandinsky.color(255,255,255))
     kandinsky.fill_rect(crateX,crateY, crateSize, crateSize,kandinsky.color(255,255,255))
+
+# Ends the game
 kandinsky.draw_string("Game Over",0,0)
 time.sleep(1)
 print("Score:",len(score))
